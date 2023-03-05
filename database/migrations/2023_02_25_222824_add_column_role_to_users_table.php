392e8113c1c6934922bc39185f1a5d5db9534f0f
rEugeneMarkov <117519379+rEugeneMarkov@users.noreply.github.com>
2023-03-06 00:50:06 +0200

    user role migration edited

--- database/migrations/2023_02_25_222824_add_column_role_to_users_table.php
+++ database/migrations/2023_02_25_222824_add_column_role_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedSmallInteger('role')->nullable();
+            $table->unsignedSmallInteger('role')->default(1)->nullable();
         });
     }
 
